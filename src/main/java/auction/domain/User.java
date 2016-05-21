12e6744ea47471e20e29fb8bf3bcfcf2f4b853f7
Guus Hamm <guushamm@gmail.com>
2016-05-21 17:20:24 +0200

    So postgres doesn't like the table name USER because it's reserved, changed the table name to users

--- src/main/java/auction/domain/User.java
+++ src/main/java/auction/domain/User.java
@@ -1,15 +1,44 @@
 package auction.domain;
 
-public class User {
+import javax.persistence.*;
+import java.io.Serializable;
 
-    private String email;
+@Entity
+@Table(name = "users")
+@NamedQueries({
+		@NamedQuery(name = "User.count", query = "select count(u) from User as u"),
+		@NamedQuery(name = "User.getAll", query = "select u from User as u"),
+		@NamedQuery(name = "User.findByEmail", query = "select u from User as u where u.email = :email")
+})
+public class User implements Serializable {
 
-    public User(String email) {
-        this.email = email;
+	@Id
+	@GeneratedValue
+	private long Id;
+	@Column(unique = true)
+	private String email;
 
-    }
+	public User(String email) {
+		this.email = email;
 
-    public String getEmail() {
-        return email;
-    }
+	}
+
+	public User() {
+	}
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		this.Id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
